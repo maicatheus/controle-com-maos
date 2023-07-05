054f18c0590c60edb99905e5857c5aee62059a18
matheus_maica <maicatheus@gmail.com>
2023-07-04 23:10:13 -0300

    Reconhecendo mão na webcam

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import cv2
+import mediapipe as mp
 
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1)
 camera = cv2.VideoCapture(0)
 
 while True:
     ret, frame = camera.read()
     
     frame_color = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_color)
+    print(results.multi_hand_landmarks)
 
     cv2.imshow('Camera', frame)
 
