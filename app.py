d94939efcce7163d041cf6ee804dfffc9cfa7e5c
matheus_maica <maicatheus@gmail.com>
2023-07-05 00:08:28 -0300

    Desenhando retangulo de referencia

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import cv2
 import mediapipe as mp
+import math
 
+
+
+def desenhar_retangulo_referencia(imagem,point):
+    x_0,y_0 = (point[0] -60),(point[1])
+    x , y = (point[0] + 60),(point[1] - 130)
+    cv2.rectangle(imagem,(x_0,y_0),(x,y),(0, 255, 0),1)
 
 def desenhar_mao(imagem, points):
     for i in range(0,21,4):
         cv2.circle(imagem, points[i], 5, (0, 255, 0), -1)
-   
+    
+def distancia_entre_pontos(ponto_origem, ponto):
+    return round(math.sqrt(abs((ponto[0] - ponto_origem[0] )**1 + (ponto[1] - ponto_origem[1])**2)),2)
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1)
@@ -28,8 +37,13 @@ while True:
                 altura, largura, _ = frame.shape
                 x, y = int(point.x * largura), int(point.y * altura)
                 hand_points.append((x,y))
-            
-            print(hand_points)
+
+            desenhar_retangulo_referencia(frame,hand_points[0])
+            print(distancia_entre_pontos(hand_points[0],hand_points[4]), end=" ")
+            print(distancia_entre_pontos(hand_points[0],hand_points[8]), end=" ")
+            print(distancia_entre_pontos(hand_points[0],hand_points[12]), end=" ")
+            print(distancia_entre_pontos(hand_points[0],hand_points[16]), end=" ")
+            print(distancia_entre_pontos(hand_points[0],hand_points[20]), end="\n")
             desenhar_mao(frame,hand_points)
 
 
