66ee18449c554253d93d6a535633c36b653e5e2d
matheus_maica <maicatheus@gmail.com>
2023-07-04 23:35:38 -0300

    Desenhando ponta dos dedos

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ import cv2
 import mediapipe as mp
 
 
+def desenhar_mao(imagem, points):
+    for i in range(0,21,4):
+        cv2.circle(imagem, points[i], 5, (0, 255, 0), -1)
+   
+
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1)
 camera = cv2.VideoCapture(0)
+
+
 
 while True:
     ret, frame = camera.read()
@@ -12,8 +19,22 @@ while True:
     frame_color = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
     results = hands.process(frame_color)
-    print(results.multi_hand_landmarks)
 
+    if results.multi_hand_landmarks:
+        for hand_landmarks  in results.multi_hand_landmarks:
+            
+            hand_points = []
+            for point in hand_landmarks.landmark:
+                altura, largura, _ = frame.shape
+                x, y = int(point.x * largura), int(point.y * altura)
+                hand_points.append((x,y))
+            
+            print(hand_points)
+            desenhar_mao(frame,hand_points)
+
+
+
+    print("\n\n\n")
     cv2.imshow('Camera', frame)
 
 
